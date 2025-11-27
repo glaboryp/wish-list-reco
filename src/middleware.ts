cf98cad2a5cbf392ab45cc004bbfa743b8f1abdb
Gloria Labory <glabory@gmail.com>
2025-11-27 18:28:02 +0100

    feat: Add Content Security Policy header to middleware and update project dependencies.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { defineMiddleware } from 'astro:middleware';
 
-export const onRequest = defineMiddleware(async (context, next) => {
+export const onRequest = defineMiddleware(async (context: any, next: any) => {
     const response = await next();
 
     const headers = response.headers;
@@ -10,6 +10,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     headers.set('X-Content-Type-Options', 'nosniff');
     headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
     headers.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+    headers.set('Content-Security-Policy', "default-src 'self'; img-src 'self' https: data:; script-src 'self' 'unsafe-inline' https://www.paypal.com; style-src 'self' 'unsafe-inline'; connect-src 'self' https://www.paypal.com https://www.sandbox.paypal.com; frame-src https://www.paypal.com https://www.sandbox.paypal.com;");
 
     return response;
 });
